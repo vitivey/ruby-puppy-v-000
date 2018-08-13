9d57be6f4ff643b2f5965b40f275642037a27ddd
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-13 21:01:18 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+def Dog
+attr_accessor :name
+@@all=[]
+
+    def initialize(name)
+      @name=name
+      @@all <<self
+    end
+    
+    def self.clear_all
+      @all=[]
+    end
+
+    def self.all
+      @all.each do |dog|
+        puts dog.name
+      end
+    end
+
+end
